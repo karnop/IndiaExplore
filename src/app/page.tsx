8d2bfaf2a4d1b3994bac2eafb2900a39212ec6d4
Manav Mahesh Sanger <karnophile@gmail.com>
2025-05-05 17:50:38 +0530

    home page commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,5 @@
+import {Button} from "@heroui/button";
+import {Search} from "lucide-react";
 
 export default function Home() {
   return (
@@ -5,6 +7,12 @@ export default function Home() {
         <div className="text w-full">
           <h1 className={"text-4xl font-bold flex w-full justify-center text-justify"}>We make your trips Safer, Better Planned, and Informative </h1>
         </div>
+          <div className="search w-full md:max-w-4xl mx-auto md:px-4">
+              <div className="flex border-2 shadow-2xl border-slate-300 rounded-lg px-4 items-center gap-2 mt-3">
+                  <Search />
+                  <input type="text" className="py-3 px-3 outline-none  w-full" placeholder={"Your Destination ..."}/>
+              </div>
+          </div>
       </div>
   );
 }
--- src/components/navBar.tsx
+++ src/components/navBar.tsx
@@ -1,11 +1,14 @@
 import React from 'react';
 import {Button} from "@heroui/button";
 import {ShipWheel, Tent} from "lucide-react";
+import Link from "next/link";
 
 function NavBar() {
     return (
         <div className={"flex justify-between max-w-4xl mx-auto mt-6 px-4 gap-2"}>
-            <div className="font-bold text-2xl flex">Indi <span className={"flex items-center w-5"}><Tent/></span> pl<span className={"flex items-center w-5"}><ShipWheel /></span>re</div>
+            <Link href={"/"} className="font-bold text-2xl flex">
+                Indi <span className={"flex items-center w-5"}><Tent/></span> pl<span className={"flex items-center w-5"}><ShipWheel /></span>re
+            </Link>
             <Button className="bg-slate-900 text-white rounded-lg py-2 w-auto cursor-pointer hover:opacity-90">Sign In</Button>
         </div>
     );
